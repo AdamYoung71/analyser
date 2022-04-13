b99705b66f5e4cbaffa44b4e9189783cd10ed7bc
Tingyi Yang <yangtingyi.me@gmail.com>
2022-04-13 22:53:08 +0100

    add new solution dva.il1

--- examples/uv.il1.c
+++ examples/uv.il1.c
@@ -1,9 +1,12 @@
 int main() {
-    z = x*3;
-    x = 3;
-    if(x>1) {
-        y = 1+x;
+    a1 = x;
+    x = 3; 
+    if(x<1) { 
+        y = 1+x; 
     }
-    x = y;
-    while(a == 1) b = 1;
+    x = y; 
+    while(x<1) {
+        a2 = 0;
+    }
+    a3 = a2;
 }
