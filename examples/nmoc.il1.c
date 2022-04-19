e4ee7f7950b8689b102624fa8fc6a72a19e3f4e9
Tingyi Yang <yangtingyi.me@gmail.com>
2022-04-19 20:56:52 +0100

    modified nmoc

--- examples/nmoc.il1.c
+++ examples/nmoc.il1.c
@@ -1,18 +1,26 @@
 int main() {
     a = 1;
     b = 2;
+    open(1);
+    open(2);
+    open(3);
     open(x);
     open(y);
     open(z);
+    open(a1);
+    close(a2);
     if(a == b) {
         close(x);
+        close(1);
     } else
     {
         close(y);
+        close(2);
     }
-    
+
     while(a < b) {
-        close(c);
+        close(z);
+        close(3);
     }
-    
+
 }
